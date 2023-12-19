4a29a40edb3fd750edf3277bb1346cf96da47376
Rafał Lalik <rafal.lalik@uj.edu.pl>
2023-12-19 12:12:59 +0100

    Add missing runtime/environment.sh script

---
+++ runtime/environment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PANDA_TRB_DISTDIR=$PANDA_TRB_BASEDIR/dist
+
+export PATH=$PANDA_TRB_DISTDIR/trbnettools/bin:$PATH
+export LIBTRBNET=$PANDA_TRB_DISTDIR/trbnettools/lib/libtrbnet.so
